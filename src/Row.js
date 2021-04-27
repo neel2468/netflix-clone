ff791aba9da8cb08c09f179116ffe0f32cb440d9
neel2468 <pneel813@gmail.com>
2021-04-27 16:28:40 -0400

    Netflix clone version 1

---
+++ src/Row.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import Youtube from  'react-youtube';
+import movieTrailer from 'movie-trailer';
+import './Row.css';
+
+const base_image_url = "https://image.tmdb.org/t/p/original";
+
+function Row({title, fetchUrl, isLargeRow }) {
+
+    const [movies,setmovies] = useState([]);
+    const [trailerUrl,setTrailerUrl] = useState("");
+    
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setmovies(request.data.results);
+        }
+        fetchData();
+    },[fetchUrl]);
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        }
+    };
+
+    const handleClick = (movie) => {
+        console.log(movie);
+        if(trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || movie?.original_title || movie?.original_name ||  "")
+            .then(url => {
+                console.log(url);
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+
+            })
+            .catch((error) => console.log(error));
+        }
+    };
+
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img 
+                        key={movie.id}
+                        onClick={() => handleClick(movie)}
+                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                        src={`${base_image_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} 
+                        alt={movie.name} />
+                ))}
+            </div>
+            { trailerUrl && <Youtube videoId={trailerUrl} opt={opts}/>}
+        </div>
+    )
+}
+
+export default Row;
